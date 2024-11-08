0927ca253f166ea650adc703a719f40ed26a750b
AlaeddineCHOURK-ALLAH <92095513+AlaeddineCHOURK-ALLAH@users.noreply.github.com>
2024-11-08 14:31:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pandas
 import matplotlib.pyplot as plt
-print("Hello World")
+def fibonnaci(n):
+  if n==0 :
+    return 0 
+  elif n==1 : 
+    return 1 
+  return fibonnaci(n-1)+fibonnaci(n-2)
